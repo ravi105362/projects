a1647b7f5db900c3e9b6bfc29a4a347c026e5137
Ravi Gupta <ravi9235705910@gmail.com>
2021-08-28 19:28:19 +0200

    Updating the doc strings and comments

--- project/upload_API/views.py
+++ project/upload_API/views.py
@@ -3,51 +3,63 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 from django.http import JsonResponse
-import json
 from .models import BlockItem
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @method_decorator(csrf_exempt, name='dispatch')
 class project(View):
 
     def post(self, request):
-        #print(request.read())
-        old_data=request.body.decode("utf-8")
         
-        print("after splitting the data")
-        old_data_1=old_data.split("text/plain")
-        new_data=old_data_1[1].split("\r\n\r\n")
+        """
+        POST method to parse a receipt and store the relevant data in DB
+        """
+        receipt_data=request.body.decode("utf-8")
+        relevant_receipt_data=receipt_data.split("text/plain")
+
+        #Data is now divided in blocks
+        data_in_blocks=relevant_receipt_data[1].split("\r\n\r\n")
+        
         block=1
         row=1
         col=1
         last_row=1
-        for i in new_data :
 
+        for each_block in data_in_blocks :
+
+            #ignoring the irrelevant data
             if(block>8):
                 break
 
-            if(i.count('\r\n')>=1):
-                row=row+i.count('\r\n')
+            # Counting the number of new lines in each_block
+            if(each_block.count('\r\n')>=1):
+                row=row+each_block.count('\r\n')
 
-            if(len(i)==0) :
+            #checking if the block is empty then ignoring the further processing
+            if(len(each_block)==0):
                 continue
-            if(i.count('-')>=2):
-                i=i.split('--')
+
+            #Removing the blocks created due to '-'
+            if(each_block.count('-')>=2):
+                each_block=each_block.split('--')
             
-            if(isinstance(i, list)):
-                if(len(i[-1])==0):
+            #checking if the new block was created due to '-' or not
+            if(isinstance(each_block, list)):
+                if(len(each_block[-1])==0):
                     continue
-                to_be_pushed=i[-1]
-                k=i[-1].split('\n')
+                to_be_pushed=each_block[-1]
+                k=each_block[-1].split('\n')
                 col_end=len(k[-1])
-            elif (isinstance(i, str)):
-                if(len(i)==0):
+            elif (isinstance(each_block, str)):
+                if(len(each_block)==0):
                     continue
-                to_be_pushed=i
-                k=i.split('\n')
+                to_be_pushed=each_block
+                k=each_block.split('\n')
                 col_end=len(k[-1])
-    
+
+            #final block information to be added 
             product_data = {
                 'current_block':block,
                 'begin_row': last_row,
@@ -57,17 +69,31 @@ class project(View):
                 'data_string': to_be_pushed,
             }
             cart_item = BlockItem.objects.create(**product_data)
+
+            #updating the block
             block=block+1
+            
+            #updating row by 2 as each block was containing two new line items
             row=row+2
+
+            #keeping the last_row value to track the begin row for next block
             last_row=row
             
         data = {
-            "message": f"New item added to Cart with id: {cart_item.id}"
+            "message": f"New item added to DB with id: {cart_item.id}"
         }
         return JsonResponse(data, status=201)
 
     def get(self, request):
+
+        """
+        GET method to get the relevant information from the DB
+        """
+
+        #count of the number of items in DB
         items_count = BlockItem.objects.count()
+
+        #getting the actual items
         items = BlockItem.objects.all()
 
         items_data = []
@@ -89,9 +115,16 @@ class project(View):
         return JsonResponse(data)
     
     def delete(self,request) :
+
+        """
+        Delete method to delete the items from DB
+        """
+
+        #getting the items in DB
         items = BlockItem.objects.all()
-        for item in items:
-            item.delete()
+
+        #deleting the items in DB
+        item.delete() for item in items
 
         data={
             'items': True,
